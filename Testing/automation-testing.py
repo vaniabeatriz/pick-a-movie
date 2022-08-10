38e529fe8f51502be73f06a1b9f80b119303e599
Ziling Guo <ziling.guo@redington.co.uk>
2022-08-10 19:54:28 +0100

    update

--- Testing/automation-testing.py
+++ Testing/automation-testing.py
@@ -1,24 +1,31 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.common.exceptions import NoSuchElementException
 
-from selenium.webdriver.common.keys import Keys
 
 # set up the path for the chromedriver
 PATH = "C:\Program Files\chromedriver.exe"
 # selecting Chrome to use as browser for selenium to run
 driver = webdriver.Chrome(PATH)
 driver.maximize_window()
-
-# open up the website
 driver.get("http://127.0.0.1:5000")
 
-
-# test for missing element, if the button doesn't exist then our page is broken
+# test for looking for home element
 
 try:
+    page_title = driver.title
+    assert page_title == "Decision Making App"
+    driver.find_element(By.LINK_TEXT, "Home")
+
+except NoSuchElementException:
+    print("cannot find home element")
+
+finally:
+    driver.close()
+# test for missing element, if the button doesn't exist/work then our page is broken
+#
+try:
+
     link = driver.find_element(By.LINK_TEXT, 'Click to get a film!')
     link.click()
 
@@ -27,7 +34,7 @@ try:
         driver.find_element(By.LINK_TEXT, 'Get another movie').click()
 
 except NoSuchElementException:
-    print("web not loading, unable to choose another movie")
+    print("the web crashed after clicking on get another movie", {x},"times")
 
 finally:
     driver.close()
