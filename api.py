014627fdabcb4d5ac4deab06bf48850e5e8d7d69
HelenBra <102252529+HelenBra@users.noreply.github.com>
2022-08-10 22:01:07 +0100

    Adding movie link

--- api.py
+++ api.py
@@ -62,10 +62,15 @@ class MovieFetcher:
         else:
             return self.no_poster_url
 
+    def get_movie_link(self):  # link to themoviedatabase's own url for picked movie
+        movie_link = f"http://www.themoviedb.org/movie/{self.random_movie_id}"
+        return movie_link # HB added in PN code please review then delete this comment
+
     def as_json(self):
         return {
             'title': self.movie_dict['title'],
             'poster_url':  self.get_poster_url(self.movie_dict['poster_path']),
+            'movie_link': self.get_movie_link(self.movie_dict['movie_link']), # HB added in this line, not 100% sure correct?
         }
 
 
