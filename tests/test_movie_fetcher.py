7f074e2cb939253e58409e5cd7461362567c0073
Ziling Guo <ziling.guo@redington.co.uk>
2022-08-14 12:50:21 +0100

    quick clean up

--- tests/test_movie_fetcher.py
+++ tests/test_movie_fetcher.py
@@ -12,10 +12,12 @@ class TestMovieFetcher(unittest.TestCase):
 
     def test_get_poster_url_empty(self):
         result = self.movie.get_poster_url(None)
-        self.assertEqual("https://www.prokerala.com/movies/assets/img/no-poster-available.webp", result)
+        self.assertEqual(
+            "https://www.prokerala.com/movies/assets/img/no-poster-available.webp", result)
 
     def test_as_json(self):
-        self.movie.movie_dict = {'id': 123, 'title': 'Amelie Poulain', 'poster_path': 'amelie.jpg'}
+        self.movie.movie_dict = {
+            'id': 123, 'title': 'Amelie Poulain', 'poster_path': 'amelie.jpg'}
         result = self.movie.as_json()
         expected = {
             'title': 'Amelie Poulain',
@@ -24,20 +26,21 @@ class TestMovieFetcher(unittest.TestCase):
         }
         self.assertEqual(expected, result)
 
-    def test_type_latest_id(self):
-        result = self.movie.latest_id()
-        message = "the latest_id variable is not an int type"
-        self.assertIs(type(result), int, message)
+    # arguably not required...
+    # def test_type_latest_id(self):
+    #     result = self.movie.latest_id()
+    #     message = "the latest_id variable is not an int type"
+    #     self.assertIs(type(result), int, message)
 
-    def test_type_random_movie_id(self):
-        result = self.movie.random_movie_id()
-        message = "the random_movie_id variable is not an int type"
-        self.assertIs(type(result), int, message)
+    # def test_type_random_movie_id(self):
+    #     result = self.movie.random_movie_id()
+    #     message = "the random_movie_id variable is not an int type"
+    #     self.assertIs(type(result), int, message)
 
-    def test_type_movie_dict(self):
-        result = self.movie.movie_dict
-        message = "the movie_dict variable is not a dict type"
-        self.assertIs(type(result), dict, message)
+    # def test_type_movie_dict(self):
+    #     result = self.movie.movie_dict
+    #     message = "the movie_dict variable is not a dict type"
+    #     self.assertIs(type(result), dict, message)
 
 
 if __name__ == '__main__':
