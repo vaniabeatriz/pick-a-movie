5f617815eb8e5ad79438eee24ed2bb0a70e1b6ed
Phoenix Null <91254984+NullPhe@users.noreply.github.com>
2022-08-14 19:31:22 +0100

    Add files via upload
    added comment re: api_key

--- src/config.py
+++ src/config.py
@@ -1,6 +1,9 @@
-# This file centralise the configuration of the variables used in the App.
+#  This file contains the configuration of the variables used in the App.
 
 api_key = "6dba5910188af21059a4441e7686f52d"
+#  Well aware this is not suitable and you should always hide your api (preferably in a env variable), but for the ease
+#  of testing, for us and the course instructors, we have the api key here as the repository is private.
+#  After 18/08/2022 this key will be disabled
 url = "https://api.themoviedb.org/3/"
 
 
